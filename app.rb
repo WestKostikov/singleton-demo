6fbc4188eba11d545eaac7ca9755052369d516d6
WestKostikov <maksrey@mail.ru>
2020-10-02 16:06:14 +0300

    Update logger

--- app.rb
+++ app.rb
@@ -1,7 +1,22 @@
 class Logger
+  def initialize
+    @f = File.open 'log.txt', 'a'
+  end
+
+  # class method
   def self.say_something
     puts "Hello"
+  end
+
+  # instance method
+  def log_something what
+    @f.puts what
   end
 end
 
 Logger.say_something
+
+Logger = Logger.new
+Logger.log_something 'Hey!'
+
+
